16a68209f14a5d59608068f507af6c2f60fb06f8
Paicodev <paedmaag@gmail.com>
2026-06-12 20:12:50 -0300

    feat: se crearon componentes para poder renderizar las rutas en el app
    -App.tsx: Aqui importamos las librerias, las rutas agrupadas con el mapeo solicitado.

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+   
+    {/* BrowserRouter envuelve la App para darle los superpoderes de ruteo */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+  );
---
+++ dashboard/src/pages/Products/ProductView.tsx
@@ -0,0 +1,3 @@
+export default function ProductView() {
+  return <h1>📦 Página de productos (ProductView)</h1>;
+}
